401d8efe765bfd3ff272ab438adc37aeedfe504b
Been777 <o7.been@gmail.com>
2024-07-19 20:20:49 +0900

    init commit

---
+++ src/test/java/com/sparta/fltpleprojectbackend/FltpleProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.fltpleprojectbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FltpleProjectBackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
